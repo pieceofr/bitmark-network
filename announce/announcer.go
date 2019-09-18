0b0298dcf4b9d8f57a5d260a87b5697989d9dea5
rachael <komimi.p@gmail.com>
2019-09-18 17:06:58 +0800

    change routing to announce

---
+++ announce/announcer.go
@@ -0,0 +1,258 @@
+// SPDX-License-Identifier: ISC
+// Copyright (c) 2014-2019 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package announce
+
+import (
+	"bitmark-network/util"
+	"encoding/binary"
+	"time"
+
+	"bitmark-network/messagebus"
+
+	"github.com/bitmark-inc/logger"
+	proto "github.com/golang/protobuf/proto"
+	peerlib "github.com/libp2p/go-libp2p-core/peer"
+)
+
+/*
+const (
+	announceInitial     = 2 * time.Minute      // startup delay before first send
+	announceRebroadcast = 7 * time.Minute      // to prevent too frequent rebroadcasts
+	announceInterval    = 11 * time.Minute     // regular polling time
+	announceExpiry      = 5 * announceInterval // if no responses received within this time, delete the entry
+)
+*/
+const (
+	announceInitial     = 1 * time.Minute      // startup delay before first send
+	announceRebroadcast = 2 * time.Minute      // to prevent too frequent rebroadcasts
+	announceInterval    = 4 * time.Minute      // regular polling time
+	announceExpiry      = 3 * announceInterval // if no responses received within this time, delete the entry
+)
+
+type announcer struct {
+	log *logger.L
+}
+
+// initialise the announcer
+func (ann *announcer) initialise() error {
+
+	log := logger.New("announcer")
+	ann.log = log
+	log.Info("initialising…")
+
+	return nil
+}
+
+// wait for incoming requests, process them and reply
+func (ann *announcer) Run(args interface{}, shutdown <-chan struct{}) {
+
+	log := ann.log
+
+	log.Info("starting… routing routine")
+
+	queue := messagebus.Bus.Announce.Chan()
+
+	delay := time.After(announceInitial)
+loop:
+	for {
+		log.Debug("waiting…")
+		select {
+		case <-shutdown:
+			break loop
+		case item := <-queue:
+			switch item.Command {
+			case "reconnect":
+				determineConnections(log)
+				log.Infof("-><- reconnect")
+			case "updatetime":
+				setPeerTimestamp(item.Parameters[0], time.Now())
+				log.Infof("-><- updatetime id:%s", string(item.Parameters[0]))
+			case "addpeer":
+				//TODO: Make sure the timestamp is from external message or  local timestamp
+				id, err := peerlib.IDFromBytes(item.Parameters[0])
+				if err != nil {
+					log.Warn(err.Error())
+					continue loop
+				}
+				timestamp := binary.BigEndian.Uint64(item.Parameters[2])
+				var listeners Addrs
+				proto.Unmarshal(item.Parameters[1], &listeners)
+				addrs := util.GetMultiAddrsFromBytes(listeners.Address)
+				if len(addrs) == 0 {
+					log.Warn("No valid listener address")
+					continue loop
+				}
+				addPeer(id, addrs, timestamp)
+				log.Infof("-><- addpeer : %s  listener: %s  timestamp: %d", id.String(), printBinaryAddrs(item.Parameters[1]), timestamp)
+				globalData.peerTree.Print(false)
+			case "self":
+				id, err := peerlib.IDFromBytes(item.Parameters[0])
+				if err != nil {
+					log.Warn(err.Error())
+					continue loop
+				}
+				var lsners Addrs
+				proto.Unmarshal(item.Parameters[1], &lsners)
+				addrs := util.GetMultiAddrsFromBytes(lsners.Address)
+				if len(addrs) == 0 {
+					log.Warn("No valid listener address")
+					continue loop
+				}
+				log.Infof("-><-  self announce data: %v  listener: %s", id, printBinaryAddrs(item.Parameters[1]))
+				setSelf(id, addrs)
+			default:
+			}
+		case <-delay: // Periodically Announce Self
+			delay = time.After(announceInterval)
+			log.Info("Announce Interval Timeup")
+			ann.process()
+		}
+	}
+}
+
+// process the annoucement and return response to client
+func (ann *announcer) process() {
+
+	log := ann.log
+
+	log.Debug("process starting…")
+
+	globalData.Lock()
+	defer globalData.Unlock()
+
+	// get a big endian timestamp
+	timestamp := make([]byte, 8)
+	binary.BigEndian.PutUint64(timestamp, uint64(time.Now().Unix()))
+
+	// announce this nodes IP and ports to other peers
+	// TODO: Need to Add RPC
+	/*
+		if globalData.rpcsSet {
+			log.Debugf("send rpc: %x", globalData.fingerprint)
+			messagebus.Bus.Broadcast.Send("rpc", globalData.fingerprint[:], globalData.rpcs, timestamp)
+		}
+	*/
+	if globalData.peerSet {
+		log.Debugf("send peer: %x", globalData.peerID)
+		addrsBinary, errAddr := proto.Marshal(&Addrs{Address: util.GetBytesFromMultiaddr(globalData.listeners)})
+		idBinary, errID := globalData.peerID.MarshalBinary()
+		if nil == errAddr && nil == errID {
+			messagebus.Bus.P2P.Send("peer", idBinary, addrsBinary, timestamp)
+		}
+	}
+	// TODO: Need to Add RPC
+	//expireRPC()
+	expirePeer(log)
+
+	if globalData.treeChanged {
+		determineConnections(log)
+		globalData.treeChanged = false
+	}
+}
+
+func determineConnections(log *logger.L) {
+	if nil == globalData.thisNode {
+		log.Errorf("determineConnections called to early")
+		return // called to early
+	}
+
+	// locate this node in the tree
+	_, index := globalData.peerTree.Search(globalData.thisNode.Key())
+	count := globalData.peerTree.Count()
+	log.Infof("determine thisNode index: %d  tree: %d  peerID: %v peerID ", index, count, globalData.peerID)
+
+	// various increment values
+	e := count / 8
+	q := count / 4
+	h := count / 2
+	log.Infof("e:%d, q:%d, h:%d", e, q, h)
+	jump := 3      // to deal with N3/P3 and too few nodes
+	if count < 4 { // if insufficient
+		jump = 1 // just duplicate N1/P1
+	}
+
+	names := [11]string{
+		"N1",
+		"N3",
+		"X1",
+		"X2",
+		"X3",
+		"X4",
+		"X5",
+		"X6",
+		"X7",
+		"P1",
+		"P3",
+	}
+
+	// compute all possible offsets
+	// if count is too small then there will be duplicate offsets
+	var n [11]int
+	n[0] = index + 1             // N1 (+1)
+	n[1] = index + jump          // N3 (+3)
+	n[2] = e + index             // X⅛
+	n[3] = q + index             // X¼
+	n[4] = q + e + index         // X⅜
+	n[5] = h + index             // X½
+	n[6] = h + e + index         // X⅝
+	n[7] = h + q + index         // X¾
+	n[8] = h + q + e + index     // X⅞
+	n[9] = index + count - 1     // P1 (-1)
+	n[10] = index + count - jump // P3 (-3)
+
+	u := -1
+deduplicate:
+	for i, v := range n {
+		if v == index || v == u {
+			continue deduplicate
+		}
+		u = v
+		if v >= count {
+			v -= count
+		}
+		node := globalData.peerTree.Get(v)
+		if nil != node {
+			peer := node.Value().(*peerEntry)
+			if nil != peer {
+				idBinary, errID := peer.peerID.Marshal()
+				pbAddr := util.GetBytesFromMultiaddr(peer.listeners)
+				pbAddrBinary, errMarshal := proto.Marshal(&Addrs{Address: pbAddr})
+				if nil == errID && nil == errMarshal {
+					messagebus.Bus.P2P.Send(names[i], idBinary, pbAddrBinary)
+					log.Infof("determine %v : %s  address: %x ", names[i], peer.peerID.String(), printBinaryAddrs(pbAddrBinary))
+				}
+			}
+		} else {
+			log.Infof("Get Nil node")
+		}
+	}
+}
+
+func expirePeer(log *logger.L) {
+	now := time.Now()
+	nextNode := globalData.peerTree.First()
+scan_nodes:
+	for node := nextNode; nil != node; node = nextNode {
+
+		peer := node.Value().(*peerEntry)
+		key := node.Key()
+
+		nextNode = node.Next()
+
+		// skip this node's entry
+		if globalData.peerID.String() == peer.peerID.String() {
+			continue scan_nodes
+		}
+		log.Debugf("PeerID: %v timestamp: %s", peer.peerID, peer.timestamp.Format(timeFormat))
+		if peer.timestamp.Add(announceExpiry).Before(now) {
+			globalData.peerTree.Delete(key)
+			globalData.treeChanged = true
+			// TODO: Send to P2P to Expire
+			//messagebus.Bus.Connector.Send("@D", peer.peerID, peer.listeners) //@D means: @->Internal Command, D->delete
+			log.Infof("Peer Expired! public key: %x timestamp: %s is removed", peer.peerID, peer.timestamp.Format(timeFormat))
+		}
+	}
+}
---
+++ announce/peerEntry.go
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: ISC
+// Copyright (c) 2014-2019 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package announce
+
+import (
+	"bitmark-network/util"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/bitmark-inc/bitmarkd/fault"
+	"github.com/bitmark-inc/bitmarkd/mode"
+	"github.com/bitmark-inc/bitmarkd/zmqutil"
+	peerlib "github.com/libp2p/go-libp2p-core/peer"
+	ma "github.com/multiformats/go-multiaddr"
+	//"github.com/bitmark-inc/bitmarkd/zmqutil"
+)
+
+//type pubkey []byte
+//type peerIDkey []byte
+type peerIDkey peerlib.ID
+
+type peerEntry struct {
+	peerID    peerlib.ID
+	listeners []ma.Multiaddr
+	timestamp time.Time // last seen time
+}
+
+// string - conversion fro fmt package
+func (p peerEntry) String() []string {
+	allAddress := []string{}
+	for _, listener := range p.listeners {
+		allAddress = append(allAddress, listener.String())
+	}
+	return allAddress
+}
+
+// SetSelf - called by the peering initialisation to set up this
+// node's announcement data
+func setSelf(peerID peerlib.ID, listeners []ma.Multiaddr) error {
+	globalData.Lock()
+	defer globalData.Unlock()
+
+	if globalData.peerSet {
+		return fault.ErrAlreadyInitialised
+	}
+	globalData.peerID = peerID
+	globalData.listeners = listeners
+	globalData.peerSet = true
+
+	addPeer(peerID, listeners, uint64(time.Now().Unix()))
+
+	globalData.thisNode, _ = globalData.peerTree.Search(peerIDkey(peerID))
+	determineConnections(globalData.log)
+
+	return nil
+}
+
+// isPeerExpiredFromTime - is peer expired from time
+func isPeerExpiredFromTime(timestamp time.Time) bool {
+	return timestamp.Add(announceExpiry).Before(time.Now())
+}
+
+// AddPeer - add a peer announcement to the in-memory tree
+// returns:
+//   true  if this was a new/updated entry
+//   false if the update was within the limits (to prevent continuous relaying)
+func AddPeer(peerID peerlib.ID, listeners []ma.Multiaddr, timestamp uint64) bool {
+	globalData.Lock()
+	rc := addPeer(peerID, listeners, timestamp)
+	globalData.Unlock()
+	return rc
+}
+
+// internal add a peer announcement, hold lock before calling
+func addPeer(peerID peerlib.ID, listeners []ma.Multiaddr, timestamp uint64) bool {
+	ts := resetFutureTimestampToNow(timestamp)
+	if isPeerExpiredFromTime(ts) {
+		return false
+	}
+
+	peer := &peerEntry{
+		peerID:    peerID,
+		listeners: listeners,
+		timestamp: ts,
+	}
+	// TODO: Take care of peer update and peer replace base on protocol of multiaddress
+	if node, _ := globalData.peerTree.Search(peerIDkey(peerID)); nil != node {
+		peer := node.Value().(*peerEntry)
+		if ts.Sub(peer.timestamp) < announceRebroadcast {
+			return false
+		}
+	}
+
+	// add or update the timestamp in the tree
+	recordAdded := globalData.peerTree.Insert(peerIDkey(peerID), peer)
+
+	globalData.log.Infof("Peer Added:  ID: %s,  sucessadd:%t  nodes in the peer tree: %d", peerID.String(), recordAdded, globalData.peerTree.Count())
+
+	// if adding this nodes data
+	if util.IDEqual(globalData.peerID, peerID) {
+		return false
+	}
+
+	if recordAdded {
+		globalData.treeChanged = true
+	}
+
+	return true
+}
+
+// resetFutureTimestampToNow - reset future timestamp to now
+func resetFutureTimestampToNow(timestamp uint64) time.Time {
+	ts := time.Unix(int64(timestamp), 0)
+	now := time.Now()
+	if now.Before(ts) {
+		return now
+	}
+	return ts
+}
+
+// GetNext - fetch the data for the next node in the ring for a given public key
+func GetNext(publicKey []byte) (peerlib.ID, []ma.Multiaddr, time.Time, error) {
+	globalData.Lock()
+	defer globalData.Unlock()
+
+	node, _ := globalData.peerTree.Search(peerIDkey(publicKey))
+	if nil != node {
+		node = node.Next()
+	}
+	if nil == node {
+		node = globalData.peerTree.First()
+	}
+	if nil == node {
+		return peerlib.ID(""), nil, time.Now(), fault.ErrInvalidPublicKey
+	}
+	peer := node.Value().(*peerEntry)
+	return peer.peerID, peer.listeners, peer.timestamp, nil
+}
+
+// GetRandom - fetch the data for a random node in the ring not matching a givpubkeyen public key
+func GetRandom(peerID peerlib.ID) (peerlib.ID, []ma.Multiaddr, time.Time, error) {
+	globalData.Lock()
+	defer globalData.Unlock()
+
+retry_loop:
+	for tries := 1; tries <= 5; tries += 1 {
+		max := big.NewInt(int64(globalData.peerTree.Count()))
+		r, err := rand.Int(rand.Reader, max)
+		if nil != err {
+			continue retry_loop
+		}
+
+		n := int(r.Int64()) // 0 … max-1
+
+		node := globalData.peerTree.Get(n)
+		if nil == node {
+			node = globalData.peerTree.First()
+		}
+		if nil == node {
+			break retry_loop
+		}
+		peer := node.Value().(*peerEntry)
+		if util.IDEqual(peer.peerID, globalData.peerID) || util.IDEqual(peer.peerID, peerID) {
+			continue retry_loop
+		}
+		return peer.peerID, peer.listeners, peer.timestamp, nil
+	}
+	return peerlib.ID(""), nil, time.Now(), fault.ErrInvalidPublicKey
+}
+
+// SendRegistration - send a peer registration request to a client channel
+func SendRegistration(client zmqutil.ClientIntf, fn string) error {
+	chain := mode.ChainName()
+
+	// get a big endian timestamp
+	timestamp := make([]byte, 8)
+	binary.BigEndian.PutUint64(timestamp, uint64(time.Now().Unix()))
+
+	return client.Send(fn, chain, globalData.peerID, globalData.listeners, timestamp)
+}
+
+// Compare - public key comparison for AVL interface
+func (p peerIDkey) Compare(q interface{}) int {
+	return util.IDCompare(peerlib.ID(p), peerlib.ID(q.(peerIDkey)))
+}
+
+// String - public key string convert for AVL interface
+func (p peerIDkey) String() string {
+	return fmt.Sprintf("%x", []byte(p))
+}
+
+// setPeerTimestamp - set the timestamp for the peer with given public key
+func setPeerTimestamp(publicKey []byte, timestamp time.Time) {
+	globalData.Lock()
+	defer globalData.Unlock()
+
+	node, _ := globalData.peerTree.Search(peerIDkey(publicKey))
+	log := globalData.log
+	if nil == node {
+		log.Errorf("The peer with public key %x is not existing in peer tree", publicKey)
+		return
+	}
+
+	peer := node.Value().(*peerEntry)
+	peer.timestamp = timestamp
+}
+
+func showIDFromByte(id []byte) {
+	ID, err := peerlib.IDFromBytes(id)
+	if err != nil {
+		globalData.log.Infof("IDFromByte Error:%v", err)
+		return
+	}
+	globalData.log.Infof("id:%x\n%s", id, ID.String())
+
+}
