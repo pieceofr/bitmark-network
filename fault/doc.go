239a58487fbb773acd27749ba43b8b04bae9c837
rachael <komimi.p@gmail.com>
2019-09-19 17:10:28 +0800

    add missing file

---
+++ fault/doc.go
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: ISC
+// Copyright (c) 2014-2019 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// Package fault - error instances
+//
+// Provides a single instance of errors to allow easy comparison
+// without having to resort to partial string matches
+package fault
